fdecc23b92e85ebc40f6ff221ebf7716cff2ecac
NavPan <navpanchhi@gmail.com>
2020-08-10 15:42:46 -0400

    first change

--- Revassess/src/main/java/com/revature/tier1/CompareStrings.java
+++ Revassess/src/main/java/com/revature/tier1/CompareStrings.java
@@ -3,6 +3,13 @@ package com.revature.tier1;
 public class CompareStrings {
 
     public static boolean compareStrings(String s1, String s2){
-        return false;
+        String s3 = s1;
+        String s4 = s2;
+        if (s3.equals(s4)){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 }
--- Revassess/src/main/java/com/revature/tier1/User.java
+++ Revassess/src/main/java/com/revature/tier1/User.java
@@ -1,4 +1,62 @@
 package com.revature.tier1;
 
 public class User {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String role;
+
+    public User(){
+        
+    }
+    
+    public User(int id, String firstName, String lastName, String password, String role){
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+        this.role = role; 
+    }
+
+    public int getId() {
+        return id;
+    }
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public String getFirstName() {
+        return firstName;
+    }
+    
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    
+    public String getLastName() {
+        return lastName;
+    }
+    
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    
+    public String getPassword() {
+        return password;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
